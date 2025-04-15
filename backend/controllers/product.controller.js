492ef41b0a1b8ba8a8d5f4882fcb53720a8e70ac
Carlos Amador <carlosamagin79@gmail.com>
2025-04-15 09:08:37 +0200

    p

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,15 +1,19 @@
 import User from "../models/user.model.js"
-import mongoose from 'mongoose';
+
 
 export const registerUser = async (req, res) => {
     const {username, email, password} = req.body;
-
+try {
     const newUser = new User({
         username,
         email,
         password,
     })
-    console.log(newUser);
 
-}
+    await newUser.save();
+
+    return res.status(200).json({success:true,data:newUser});
+} catch (error) {
+    console.log(error)
+}}
 
