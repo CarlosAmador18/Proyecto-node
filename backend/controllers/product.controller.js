bdf3a12f8cbdb71649520d0786c1e54f1e945088
Carlos Amador <carlosamagin79@gmail.com>
2025-04-15 08:46:34 +0200

    p

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,56 +1,15 @@
 import User from "../models/user.model.js"
 import mongoose from 'mongoose';
 
-export const getUsers =  async (req, res) => {
-    try {
-       const products = await User.find({}); 
-       res.status(200).json({success:true, data:products})
-    } catch (error) {
-        console.log("error in fetching the products: ", error.message)
-        res.status(500).json({success:false, message:"Server error"})
-      }
-} 
+export const registerUser = async (req, res) => {
+    const {username, email, password} = req.body;
 
-export const createUser = async (req,res) => {
-    const user = req.body
-    if (!user.name || !user.password || !user.email) {
-        return res.status(400).json({ success: false, message: "Please provide all fields" })
-    }
-
-    const newUser = new User(user)
-
-    try {
-        await newUser.save();
-        res.status(200).json({ success: true, data: newUser });
-    } catch (error) {
-        console.error("User error", error.message)
-    }
-}
-
-export const updateUser = async (req, res) => {
-    const { id } = req.params;
-    const user = req.body;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ success: false, message: "Invalid Product Id" })
-    }
-
-    try {
-        const updatedUser = await User.findByIdAndUpdate(id, user, { new: true });
-        res.status(200).json({ sucess: true, data: updatedUser })
-    } catch (error) {
-        res.status(500).json({ sucess: true, message: "Server error" })
-    }
-}
-
-export const deleteUser =  async (req, res) => {
-    const { id } = req.params
-    try {
-        await User.findByIdAndDelete(id)
-        res.status(200).json({ sucess: true, message: "User deleted" })
-    } catch (error) {
-        res.status(400).json({ sucess: false, message: "User not found" })
-    }
+    const newUser = new User({
+        username,
+        email,
+        password,
+    })
+    console.log(newUser);
 
 }
 
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,13 +1,8 @@
 import express from "express";
-
-import { getUsers,createUser, updateUser, deleteUser } from "../controllers/product.controller.js";
+import { registerUser } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
-router.get("/", getUsers)
-router.post("/", createUser)
-// put es si quieres actualizar todas las fields, si solo quieres actualizar algunas, es patch
-router.put("/:id", updateUser)
-router.delete("/:id", deleteUser)
+router.post("/register", registerUser)
 
 export default router;
