69e368ffbabbf82a0a908dfb2be7296d499fad62
Carlos Amador <carlosamagin79@gmail.com>
2025-04-15 15:25:48 +0200

    p

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { registerUser,loginUser, logoutUser, profileUser } from "../controllers/user.controller.js";
+import { 
+    registerUser,
+    loginUser, 
+    logoutUser, 
+    profileUser
+ } from "../controllers/user.controller.js";
+ import { authRequired } from "../middlewares/validateToken.js";
+
 
 
 const router = express.Router();
@@ -7,6 +14,6 @@ const router = express.Router();
 router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.post("/logout", logoutUser)
-router.get("/profile",profileUser)
+router.get("/profile",authRequired, profileUser)
 
 export default router;
