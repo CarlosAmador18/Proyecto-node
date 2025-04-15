43fa1b04528b7445b070f81209012b0d3d540987
Carlos Amador <carlosamagin79@gmail.com>
2025-04-15 16:32:22 +0200

    p

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -14,10 +14,8 @@ export const registerUser = async (req, res) => {
           message: ["The email is already in use"],
         });
   
-      // hashing the password
       const passwordHash = await bcrypt.hash(password, 10);
   
-      // creating the user
       const newUser = new User({
         username,
         email,
@@ -30,11 +28,7 @@ export const registerUser = async (req, res) => {
         id: userSaved._id,
       });
   
-      res.cookie("token", token, {
-        httpOnly: process.env.NODE_ENV !== "development",
-        secure: true,
-        sameSite: "none",
-      });
+      res.cookie("token", token);
   
       res.json({
         id: userSaved._id,
@@ -68,11 +62,7 @@ export const registerUser = async (req, res) => {
         username: userFound.username,
       });
   
-      res.cookie("token", token, {
-        httpOnly: process.env.NODE_ENV !== "development",
-        secure: true,
-        sameSite: "none",
-      });
+      res.cookie("token", token);
   
       res.json({
         id: userFound._id,
@@ -86,13 +76,21 @@ export const registerUser = async (req, res) => {
 
   export const logoutUser = async (req, res) => {
     res.cookie("token", "", {
-      httpOnly: true,
-      secure: true,
       expires: new Date(0),
     });
     return res.sendStatus(200);
   };
 
-  export const profileUser = (req,res) => {
-    res.send('profile')
+  export const profileUser = async (req,res) => {
+    const userFound = await User.findById(req.user.id)
+
+    if (!userFound) return res.status(404).json({message: "User not found"})
+
+      return res.json({
+        id:userFound._id,
+        username:userFound.username,
+        email:userFound.email,
+        createdAt:userFound.createdAt,
+        updatedAt:userFound.updatedAt
+      })
   }
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js'
-
+import cookieParser from 'cookie-parser'
 import userRoutes from "./routes/user.route.js"
 
+const PORT = process.env.PORT || 5000;
 dotenv.config()
 
 const app = express()
 
-app.use(express.json()); // allows us to accept JSON data in the req.body
-
-const PORT = process.env.PORT || 5000;
-
+app.use(cookieParser()) 
+app.use(express.json()); 
 app.use("/api/users",userRoutes);
 
 app.listen(PORT, () => {
