f323eb6a7548dd4f15f586a0977b60d9255456ec
Carlos Amador <carlosamagin79@gmail.com>
2025-04-17 16:48:30 +0200

    p

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 function RegisterPage() {
+  const { register } = useForm();
+
   return (
     <div>
-      
+      <form action="">
+        <input type="text" {...register("username", { required: true })} />
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+      </form>
     </div>
-  )
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
